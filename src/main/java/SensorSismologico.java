38e78e8036fcc139f3c46717acaa8f0bed86b62b
nicola-19010 <n.pacheco02@ufromail.cl>
2022-09-21 10:06:56 -0300

    tercer commit

--- src/main/java/SensorSismologico.java
+++ src/main/java/SensorSismologico.java
@@ -1,10 +1,40 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class SensorSismologico {
     public static void main(String[] args) {
         double gradoSismoSinTruncar = ( (Math.random() * ( 9.5 - 0.5 )) + 0.5);
-        double gradosSismosTruncados = Math.round(gradoSismoSinTruncar);
-        System.out.println(gradosSismosTruncados);
+
+
+
 
     }
+
+    private static int validarIngreso( ){
+        Scanner teclado = new Scanner(System.in);
+        int entrada;
+        do {
+            try {
+                entrada = teclado.nextInt();
+                break;
+            } catch (Exception e) {
+                System.out.println("Ingrese una opción válida:");
+                teclado.nextLine();
+            }
+        } while (true);
+        return entrada;
+    }
+
+    public static int ValidarDiasValidos(int entrada) {
+        if (entrada < 1 || entrada > 365){
+            return validarIngreso();
+        }else {
+            return entrada;
+        }
+    }
+
     private static double [][] rellenarMatriz(int dias) {
         double [][] matrizRellena = new double[dias][24];
         double gradoSismoSinTruncar;
@@ -13,9 +43,16 @@ public class SensorSismologico {
         for (int i = 0; i < matrizRellena.length; i++) {
             for (int j = 0; j < matrizRellena[i].length; j++) {
                 gradoSismoSinTruncar = ( (Math.random() * ( 9.5 - 0.5 )) + 0.5);
-                gradosSismosTruncados = Math.round(gradoSismoSinTruncar);
+
+                //para truncar
+                BigDecimal gradosSismo = new BigDecimal(gradoSismoSinTruncar);
+                BigDecimal rounded = gradosSismo.setScale(1, RoundingMode.FLOOR);
+                gradosSismosTruncados = rounded.doubleValue();
+
+                //rellenar la matriz
+                matrizRellena[i][j] = gradosSismosTruncados;
             }
         }
-        return null;
+        return matrizRellena;
     }
 }
