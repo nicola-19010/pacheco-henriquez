1cb68e6561018152c045453ddef6c24c7e680348
Marcelo <m.henriquez11@ufromail.cl>
2022-09-21 10:06:23 -0300

    Tercer commit

--- src/main/java/SensorSismologico.java
+++ src/main/java/SensorSismologico.java
@@ -4,51 +4,41 @@ import java.math.RoundingMode;
 import java.util.InputMismatchException;
 
 public class SensorSismologico {
-    private static int pedirDias() {
-        int dias;
-
-        while(true) {
-            System.out.println("Por favor ingrese la cantidad de días");
-            Scanner scanner = new Scanner(System.in);
-
-            try {
-                dias = scanner.nextInt();
-            }catch(InputMismatchException e) {
-                continue;
-            }
-
-            if(dias > 1 && dias < 32) {
-                return dias;
-            }
-        }
+    public static void main(String[] args) {
+        mostrarMenu();
     }
 
-    private static void MostrarMenu() {
+    private static void mostrarMenu() {
         System.out.println("------MENU------");
         System.out.println("1. ¿Qué intensidad, qué día y a qué hora ocurrió el sismo?");
         System.out.println("2. ¿Cuántos días en total se registraron sismos >= 5.5 grados?");
         System.out.println("3. ¿Hay alerta de escalada sísmica?");
         System.out.println("4. ¿Hay alerta de enjambre sísmico?");
+        System.out.println("5. Salir del sistema.");
         int opcionMenu = validarIngreso();
+        opcionesMenu(validarIngreso());
     }
 
-    private static void opcionesMenu(int opcion,){
+    private static void opcionesMenu(int opcion){
         switch (opcion){
             case 1:
             case 2:
             case 3:
-                calcularMasDe4Sismos();
+                hayAlertaDeEscalada();
+            case 4:
+            case 5:
+                salirDelSistema();
         }
     }
 
-    public static int [] calcularMasDe4Sismos(double [][] matrizRellena, int dias) {
+    public static int[] hayAlertaDeEscalada(double [][] matrizLlena, int dias) {
         int aux;
         int vecesPorDia = 0;
         int [] matrizMayorCuatro = new int [dias];
 
-        for(int i = 0; i < matrizRellena.length; i++) {
-            for(int j = 0; j < matrizRellena[i].length; j++ ) {
-                if(matrizRellena[i][j] >= 4.0) {
+        for(int i = 0; i < matrizLlena.length; i++) {
+            for(int j = 0; j < matrizLlena[i].length; j++ ) {
+                if(matrizLlena[i][j] >= 4.0) {
                     vecesPorDia++;
                 }
             }
@@ -60,18 +50,21 @@ public class SensorSismologico {
         return matrizMayorCuatro;
     }
 
-    private static int validarIngreso() {
-        Scanner teclado = new Scanner(System.in);
-        int entrada;
-        do {
-            try {
-                entrada = teclado.nextInt();
+    private static void salirDelSistema(){
+        System.out.println("¿Está seguro/a que desea salir del sistema?");
+        System.out.println("1. Si");
+        System.out.println("2. No");
+        int opcionIngresada = validarIngreso();
+        opcionesSalirDelSistema(validarIngreso());
+    }
+
+    private static void opcionesSalirDelSistema(int opcionIngresada){
+        switch (opcionIngresada){
+            case 1:
+                System.out.println("Ha salido del sistema.");
                 break;
-            } catch (Exception e) {
-                System.out.println("Ingrese una opción válida:");
-                teclado.nextLine();
-            }
-        } while (true);
-        return entrada;
+            case 2:
+                mostrarMenu();
+        }
     }
 }
