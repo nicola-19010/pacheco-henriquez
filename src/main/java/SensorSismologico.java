c452fb6e4c4da8af8115890915622b681357cd16
nicola-19010 <n.pacheco02@ufromail.cl>
2022-09-21 09:34:47 -0300

    segundo commit

--- src/main/java/SensorSismologico.java
+++ src/main/java/SensorSismologico.java
@@ -1,2 +1,21 @@
 public class SensorSismologico {
+    public static void main(String[] args) {
+        double gradoSismoSinTruncar = ( (Math.random() * ( 9.5 - 0.5 )) + 0.5);
+        double gradosSismosTruncados = Math.round(gradoSismoSinTruncar);
+        System.out.println(gradosSismosTruncados);
+
+    }
+    private static double [][] rellenarMatriz(int dias) {
+        double [][] matrizRellena = new double[dias][24];
+        double gradoSismoSinTruncar;
+        double gradosSismosTruncados;
+
+        for (int i = 0; i < matrizRellena.length; i++) {
+            for (int j = 0; j < matrizRellena[i].length; j++) {
+                gradoSismoSinTruncar = ( (Math.random() * ( 9.5 - 0.5 )) + 0.5);
+                gradosSismosTruncados = Math.round(gradoSismoSinTruncar);
+            }
+        }
+        return null;
+    }
 }
